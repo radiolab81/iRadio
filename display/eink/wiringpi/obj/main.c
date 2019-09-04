96e0f860a7cb99663de53279380cd7ef11be02e2
BM45 <48355256+BM45@users.noreply.github.com>
2019-09-04 14:54:01 +0200

    Update main.c

--- display/eink/wiringpi/obj/main.c
+++ display/eink/wiringpi/obj/main.c
@@ -40,10 +40,10 @@ void  Handler(int signo)
 
 int main (void)
 {
-	// warte bis vlc,spi... aktiv sind
-	delay(3000);
+    // warte bis vlc,spi... aktiv sind
+    delay(3000);
 
-	// Initialisierung
+    // Initialisierung
     DEV_ModuleInit();
 
     // Exception handling: ctrl + c
@@ -57,15 +57,15 @@ int main (void)
     DEV_Delay_ms(500);
 
     // Datenhalter für Sendername, Titel,... aus vlc
-	char title[64], title_old[64];
-	char now[64], now_old[64];
-	char genre[64];
-	char bitrate[64];
+    char title[64], title_old[64];
+    char now[64], now_old[64];
+    char genre[64];
+    char bitrate[64];
 
-	FILE *text, *text_now_playing, *text_genre, *text_bitrate;
+    FILE *text, *text_now_playing, *text_genre, *text_bitrate;
 
 
-	//Create a new image cache
+    //Create a new image cache
     UBYTE *BlackImage;
     UWORD Imagesize = ((EPD_WIDTH % 8 == 0)? (EPD_WIDTH / 8 ): (EPD_WIDTH / 8 + 1)) * EPD_HEIGHT;
     if((BlackImage = (UBYTE *)malloc(Imagesize)) == NULL) {
@@ -79,9 +79,9 @@ int main (void)
 	// Daemonbetrieb
   	while (1) {
 
-			// zyklisch Daten von vlc holen
+		// zyklisch Daten von vlc holen
        	   	text = popen("echo \"info\" | nc 127.0.0.1 9294 -N | grep title:", "r");
-		    text_now_playing = popen("echo \"info\" | nc 127.0.0.1 9294 -N | grep now_playing:", "r");
+	        text_now_playing = popen("echo \"info\" | nc 127.0.0.1 9294 -N | grep now_playing:", "r");
     		text_genre = popen("echo \"info\" | nc 127.0.0.1 9294 -N | grep genre:", "r");
     		text_bitrate = popen("echo \"info\" | nc 127.0.0.1 9294 -N | grep Bitrate:", "r");
 
@@ -112,50 +112,50 @@ int main (void)
     		genre[strlen(genre)-1]='\0';
     		bitrate[strlen(bitrate)-1]='\0';
 
-            // haben sich Sendername oder Titel geändert? 
+                // haben sich Sendername oder Titel geändert? 
     		if ( (strcmp(title,title_old)!=0) || (strcmp(now,now_old)!=0) ) {
                 
-  				 // Display aus dem Tiefschlaf holen
-			     if(EPD_Init(lut_full_update) != 0) {
-			        printf("e-Paper init failed\r\n");
-			     }
-    			 EPD_Clear();
+  		   // Display aus dem Tiefschlaf holen
+		     if(EPD_Init(lut_full_update) != 0) {
+		        printf("e-Paper init failed\r\n");
+		     }
+    		     EPD_Clear();
 
-				 // Auf Displaybuffer schreiben
-				 Paint_Clear(WHITE);				
-				 Paint_DrawString_EN(10, 10, title+1, &Font20, WHITE, BLACK);
-  				 Paint_DrawLine(0, 40, EPD_HEIGHT, 40, BLACK, LINE_STYLE_SOLID, DOT_PIXEL_1X1);  
-				 Paint_DrawString_EN(20, 60, now+1, &Font16, WHITE, BLACK);
-				 Paint_DrawString_EN(20, 80, genre+1, &Font16, WHITE, BLACK);
-			 	 Paint_DrawString_EN(20, 100, bitrate+1, &Font16, WHITE, BLACK);
+		     // Auf Displaybuffer schreiben
+		     Paint_Clear(WHITE);				
+		     Paint_DrawString_EN(10, 10, title+1, &Font20, WHITE, BLACK);
+  		     Paint_DrawLine(0, 40, EPD_HEIGHT, 40, BLACK, LINE_STYLE_SOLID, DOT_PIXEL_1X1);  
+		     Paint_DrawString_EN(20, 60, now+1, &Font16, WHITE, BLACK);
+		     Paint_DrawString_EN(20, 80, genre+1, &Font16, WHITE, BLACK);
+		     Paint_DrawString_EN(20, 100, bitrate+1, &Font16, WHITE, BLACK);
 
-				 // Display aktualisieren und in den Tiefschlaf legen !!! LEBENSZEITVERLAENGERUNG !!!
-                 EPD_Display(BlackImage);
-				 EPD_Sleep();
+		     // Display aktualisieren und in den Tiefschlaf legen !!! LEBENSZEITVERLAENGERUNG !!!
+                     EPD_Display(BlackImage);
+		     EPD_Sleep();
                 }
 
     		strcpy(title_old,title);
     		strcpy(now_old,now);
 
-			if (text != NULL) {
+		if (text != NULL) {
     			pclose(text);
-				text = NULL;
-			}
+			text = NULL;
+		}
 
-			if (text_now_playing != NULL) {
-	    			pclose(text_now_playing);
-					text_now_playing = NULL;
-			}
+		if (text_now_playing != NULL) {
+	    		pclose(text_now_playing);
+			text_now_playing = NULL;
+		}
 
-			if (text_genre != NULL ) {
-	    			pclose(text_genre);
-					text_genre = NULL;
-			}
+		if (text_genre != NULL ) {
+	    		pclose(text_genre);
+			text_genre = NULL;
+		}
 
-			if (text_bitrate != NULL ) {
-	    			pclose(text_bitrate);
-					text_bitrate = NULL;
-			}
+		if (text_bitrate != NULL ) {
+	    		pclose(text_bitrate);
+			text_bitrate = NULL;
+		}
 
     		delay(500);
 	}
@@ -164,5 +164,3 @@ int main (void)
     free(BlackImage);
     BlackImage = NULL;
 }
-
-
